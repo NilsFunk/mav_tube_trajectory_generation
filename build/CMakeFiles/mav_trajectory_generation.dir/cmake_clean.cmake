d6b30bbe89a4e6daae0ed3294849ec7d8e397ac0
nilsiism <funkn@student.ethz.ch>
2018-08-20 15:20:05 +0100

    initial setup

---
+++ build/CMakeFiles/mav_trajectory_generation.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mav_trajectory_generation.dir/src/motion_defines.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/polynomial.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/segment.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/timing.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/trajectory.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/vertex.cpp.o"
+  "CMakeFiles/mav_trajectory_generation.dir/src/rpoly/rpoly_ak1.cpp.o"
+  "libmav_trajectory_generation.pdb"
+  "libmav_trajectory_generation.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mav_trajectory_generation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mav_trajectory_generation_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mav_trajectory_generation_lib.
+# This may be replaced when dependencies are built.
